7065d2e3cefe0ab7ff4b07b2139414283d3c780c
Sther <sther@carcara.tech>
2025-06-30 14:13:13 -0300

    chore: Nuxt setup + welcome UI + empty JSON

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="hello">
+    Hello Nuxt!!
   </div>
 </template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt'],
+  ssr: true,
+  css: [],
+  runtimeConfig: {
+    public: {
+      apiUrl: 'http://localhost:3001',
+    },
+  },
 })
